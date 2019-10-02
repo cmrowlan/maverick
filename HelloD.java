0cca675bcde4e29c024592c137fc9477f9449ba0
cmrowlan <56093389+cmrowlan@users.noreply.github.com>
2019-10-02 15:30:18 -0500

    Add files via upload

---
+++ HelloD.java
@@ -0,0 +1,11 @@
+public class HelloD{
+    public static void main(String[] args) {
+    int x = 1;
+    do{
+        System.out.println("Hello World");
+        x++;}
+     while (x<=10);
+    
+        
+    }
+}
