e00116a222f8a3a50166fe158f223b974519d67a
cmrowlan <56093389+cmrowlan@users.noreply.github.com>
2019-10-09 19:27:17 -0500

    update to proj

--- Mav.java
+++ Mav.java
@@ -1,33 +1,27 @@
 import java.util.Scanner;
-
 public class Mav{
-    public static int q;
-    public static int finalNum = 0;
     public static String add = "add";
     public static String sub = "subtract";
     public static String mult = "multiply";
     public static String div = "divide";
     public static String mod = "modulus";
     public static String exit = "exit";
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        // Welcome message and first number entry
+        initial();
+     }
+    public static void initial() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the Mav Calculator" + '\n' + "Please enter your first number now: ");
-        int q = scan.nextInt();
-
-        // Action menu seen here
-        System.out.println("ADD +" + '\n' + "SUBTRACT - " + '\n' + "MUTLIPLY * " + '\n' + "DIVIDE / " + '\n'
-                + "MODULUS % " + '\n' + "EXIT e" + '\n' + "Please enter your desired operation: ");
-            readIn(q);
+        if (sc.hasNextInt()) {
+            int q = sc.nextInt();
+            menu(q);
+        }
     }
-        // Read in action here
-    public static void readIn(int q){
-        int x = 1;
-        Scanner scan = new Scanner(System.in);
-        String act = scan.next();
-    first:
-        do { 
+    public static void menu(int q) {
+        System.out.println("MENU:" + '\n' + "ADD +" + '\n' + "SUBTRACT - " + '\n' + "MUTLIPLY * " + '\n' + "DIVIDE / " + '\n'
+        + "MODULUS % " + '\n' + "EXIT e" + '\n' + "Please enter your desired operation: ");
+        Scanner sc = new Scanner(System.in);
+        String act = sc.next();
        if (act.equalsIgnoreCase(add))
                  doAdd(q);
         if (act.equalsIgnoreCase(sub))
@@ -44,39 +38,51 @@ public class Mav{
             }
         else {
             System.out.println("Please enter a valid input");
-            x++;
-            readIn(0); 
-        } 
-        
-        while (x<15) {
-            System.out.println("hi");
+            initial(); 
         }
-     }
-    public static void doSub(int x) {
+        
+    }
+    public static void doSub(int q) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter your second number now: ");
         int y = scan.nextInt();
         int answer = q - y;
+        System.out.println("Your answer is: " + answer);
+        initial();
     }
-    public static void doMult(int x) {
-        
+    public static void doMult(int q) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your second number now: ");
+        int y = scan.nextInt();
+        int answer = q * y;
+        System.out.println("Your answer is: " + answer);     
+        initial();
     }
-    public static void doDiv(int x) {
-        
+    public static void doDiv(int q) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your second number now: ");
+        int y = scan.nextInt();
+        int answer = q / y;
+        System.out.println("Your answer is: " + answer);     
+        initial();
     }
-    public static void doMod(int x) {
-        
+    public static void doMod(int q) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your second number now: ");
+        int y = scan.nextInt();
+        int answer = q % y;
+        System.out.println("Your answer is: " + answer);     
+        initial();
     }
     public static void doAdd(int q) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter your second number now: ");
         int y = scan.nextInt();
         int answer = q + y;
-        System.out.println(answer);
-
-        
-    
+        System.out.println("Your answer is: " + answer);     
+        initial();
+    }
         
     }
-}
+
 
