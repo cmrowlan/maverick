4a1a9954bbf2d153493b447dcb6f197c9e11a073
cmrowlan <56093389+cmrowlan@users.noreply.github.com>
2019-12-12 18:56:40 -0600

    continue lessons

--- Node.js/node-course/notes-app/app.js
+++ Node.js/node-course/notes-app/app.js
@@ -1,3 +1,55 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
-const msg = getNotes()
-console.log(msg)
+
+//Customize version
+yargs.version('1.1.0')
+
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note.',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'This is the body of the note.',
+            demandOption: true,
+            type: 'string'
+        },
+    },
+    handler: function(argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+
+})
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Removing a note.',
+    handler: function() {
+        console.log('Removing the note')
+    }
+})
+//create list command
+yargs.command({
+    command: 'list',
+    describe: 'Listing the notes.',
+    handler: function(){
+        console.log('Listing the notes')
+    }
+})
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'Reads the notes.',
+    handler: function() {
+        console.log('Reading the notes')
+    }
+})
+console.log(process.argv)
+console.log(yargs.argv)
