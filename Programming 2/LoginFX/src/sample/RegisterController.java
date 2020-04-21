e4cbe62091d8844f884ac9bb61d4d6bb3b7dc3c7
cmrowlan <56093389+cmrowlan@users.noreply.github.com>
2020-04-20 19:27:01 -0500

    working ui -added change pwd and updated home screen

--- Programming 2/LoginFX/src/sample/RegisterController.java
+++ Programming 2/LoginFX/src/sample/RegisterController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ import java.io.IOException;
 public class RegisterController {
     public Button btnRegister;
     public Button btnCancel;
+    public TextField txtName;
 
     public void cancel(ActionEvent actionEvent) throws IOException {
         Node node = (Node) actionEvent.getSource();
