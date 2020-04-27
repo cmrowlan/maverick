5d32c4a63dc13facb239faf0dffe4c08afc0bb83
cmrowlan <56093389+cmrowlan@users.noreply.github.com>
2020-04-27 10:17:56 -0500

    started work on forgot pwd -needs change pwd

---
+++ Programming 2/LoginFX/src/sample/ForgotPwdChangeController.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class ForgotPwdChangeController {
+    User currentUser = ForgotPwdController.currentUser;
+
+    public void submit(ActionEvent actionEvent) {
+
+    }
+}
--- Programming 2/LoginFX/src/sample/ForgotPwdController.java
+++ Programming 2/LoginFX/src/sample/ForgotPwdController.java
@@ -1,16 +1,32 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class ForgotPwdController {
+    @FXML
+    public TextField txtEmail;
+    @FXML
+    public Label lblError;
+    public static User currentUser = null;
 
     public void cancel(ActionEvent actionEvent) throws IOException {
         Common.loadWindow(actionEvent, "Home.fxml");
     }
 
     public void send(ActionEvent actionEvent) throws IOException {
-        Common.loadWindow(actionEvent, "Home.fxml");
+        for(int x = 0; x<Main.users.size(); x++){
+            if(Main.users.get(x).getEmail().equals(txtEmail.getText())){
+                currentUser = Main.users.get(x);
+                Common.loadWindow(actionEvent, "ForgotPwdChangeController.fxml");
+            }else{
+                lblError.setText("No account found with Email!");
+            }
+        }
+
     }
 }
