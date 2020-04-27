05b929c29f52bdf09a5a2417180fba76ea2277c6
cmrowlan <56093389+cmrowlan@users.noreply.github.com>
2020-04-27 16:17:40 -0500

    continued work on forgot pwd -needs change pwd

--- Programming 2/LoginFX/src/sample/ForgotPwdController.java
+++ Programming 2/LoginFX/src/sample/ForgotPwdController.java
@@ -3,30 +3,41 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public class ForgotPwdController {
     @FXML
-    public TextField txtEmail;
+    public TextField txtUsername;
     @FXML
     public Label lblError;
-    public static User currentUser = null;
+    @FXML
+    public PasswordField txtCPwd;
 
+    public static User currentUser = null;
+    Hash hash = new Hash();
     public void cancel(ActionEvent actionEvent) throws IOException {
-        Common.loadWindow(actionEvent, "Home.fxml");
+        Common.loadWindow(actionEvent, "Login.fxml");
     }
 
-    public void send(ActionEvent actionEvent) throws IOException {
+    public void send(ActionEvent actionEvent) throws IOException, NoSuchAlgorithmException {
         for(int x = 0; x<Main.users.size(); x++){
-            if(Main.users.get(x).getEmail().equals(txtEmail.getText())){
+            if(Main.users.get(x).getUsername().equals(txtUsername.getText())){
                 currentUser = Main.users.get(x);
-                Common.loadWindow(actionEvent, "ForgotPwdChangeController.fxml");
+                byte[] salt = hash.getSalt();
+                String pwd = hash.getHash(txtCPwd.getText(), salt);
+                Main.users.get(x).setPwd(pwd);
+                String saltString = Base64.getEncoder().encodeToString(salt);
+                Main.users.get(x).setSalt(saltString);
             }else{
-                lblError.setText("No account found with Email!");
+                lblError.setText("No account found with that Username!");
             }
         }
+
 
     }
 }
