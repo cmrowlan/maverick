cb55092d1f7e03234df636e15e6bf5b6ef9e5db8
cmrowlan <56093389+cmrowlan@users.noreply.github.com>
2020-04-28 17:04:18 -0500

    video 17 node course

--- Node.js/node-course/notes-app/app.js
+++ Node.js/node-course/notes-app/app.js
@@ -1,8 +1,40 @@
-const validator = require('validator')
+const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-console.log(getNotes())
+yargs.version('1.1.0')
 
-console.log(validator.isEmail('chase@outlook.com'))
 
-console.log(validator.isURL('chase.com'))
+//add, remove, read, list
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function (){
+        console.log('Adding a new note')
+    }
+})
+//remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing the note')
+    }
+})
+yargs.command({
+    command: 'list',
+    describe: 'List the notes',
+    handler: function () {
+        console.log('Listing a note')
+    }
+})
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading the note')
+    }
+})
+
+console.log(yargs.argv)
